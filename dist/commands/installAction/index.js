5c73b7541d96d8578ddacdf2fcad978ee148a07e
Thezero <yonghu520@outlook.com>
2022-03-25 19:06:55 +0800

    fix: detaul

--- dist/commands/installAction/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.installAction = void 0;
-const installAction = async (install) => {
-    console.log('🔥log=>index=>6:install.name:%o', install.name);
-    if (install.name) {
-    }
-};
-exports.installAction = installAction;
--- dist/config/path.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.libsoutputPath = exports.libsEnterPath = exports.enterPath = exports.outputPath = exports.rootPath = void 0;
-const path_1 = require("path");
-exports.rootPath = (0, path_1.resolve)(__dirname, '..', '..');
-exports.outputPath = (0, path_1.resolve)(exports.rootPath, 'dist');
-exports.enterPath = (0, path_1.resolve)(exports.rootPath, 'package');
-exports.libsEnterPath = (0, path_1.resolve)(exports.enterPath, 'libs');
-exports.libsoutputPath = (0, path_1.resolve)(exports.outputPath, 'libs');
